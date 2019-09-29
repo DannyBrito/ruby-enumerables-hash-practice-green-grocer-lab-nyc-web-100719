e0d6eac13aab1f4974a68f162f10307b027b8dad
Danny Brito <britodanny1997@gmail.com>
2019-09-29 23:51:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,18 +1,4 @@
-def consolidate_cart(cart)
-  # code here
-end
 
-def apply_coupons(cart, coupons)
-  # code here
-end
-
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
 
 ###################################################################
 def consolidate_cart(cart) # consolidate_cart - method (return organize cart with counts)
